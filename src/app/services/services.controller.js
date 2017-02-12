0aacbb7d8b6d37a18e67869a695a3fed9a2e044a
Aslesha <pansare.aslesha@gmail.com>
2017-02-11 21:41:59 -0500

    services section- buttons done

--- src/app/services/services.controller.js
+++ src/app/services/services.controller.js
@@ -1,6 +1,34 @@
-function servicesCtrl() {
-  var vm = this;
+function servicesController() {
+  const vm = this;
   vm.message = 'HELLO WORLD';
+  vm.desc = '';
+
+  const listOfServices = [
+    'Manual Therapy',
+    'Diet and Nutrition',
+    'Magnet Therapy',
+    'Massage Therapy',
+    'Pre/Post Natal Exercises',
+    'Therapeutic Exercises',
+    'Sports Injuries',
+    'Kinesio Taping',
+    'Ultrasound Treatment',
+    'Electrical Stimulation',
+    'Acute and Chronic Pain Management',
+    'Mechanical Traction'
+  ];
+
+  vm.getServicesNames = function () {
+    return listOfServices;
+  };
+
+  vm.getDesc = function () {
+    return null;
+  };
+
+  vm.disableDesc = function () {
+    return vm.desc !== '';
+  };
 }
 
-angular.module('clinicalWebsite').controller('servicesCtrl', servicesCtrl);
+angular.module('clinicalWebsite').controller('servicesController', servicesController);
--- src/app/services/services.directive.js
+++ src/app/services/services.directive.js
@@ -6,7 +6,7 @@ angular.module('clinicalWebsite')
         //@ reads the attribute value, = provides two-way binding, & works with functions
         title: '@'         },
       templateUrl: 'app/services/services.tpl.html',
-      controller: 'servicesCtrl', //Embed a custom controller in the directive
-      link: function ($scope, element, attrs) { } //DOM manipulation
-    }
+      controller: 'servicesController as sCtrl',
+      //link: function ($scope, element, attrs) { } //DOM manipulation
+    };
 });
